2141a2a6a005f1f16693069243791a76349fe73a
Genthellmann <thellmjo1996@gmail.com>
2022-05-20 11:18:23 +0200

    first upload

---
+++ config/db_config.js
@@ -0,0 +1,13 @@
+// will contain configuration for information like the database credentials and
+// the rows we want to show per page when we paginate results.
+
+const config = {
+    db: {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'trendsDB'
+    },
+};
+
+module.exports = config;
